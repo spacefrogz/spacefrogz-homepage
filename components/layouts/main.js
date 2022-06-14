5a690091f45a091ed19955673a9d81d040d02cd4
Horea Crisan <h22ea@icloud.com>
2022-06-14 15:37:22 +0300

    Created web app, integrated main homepage with basic components

---
+++ components/layouts/main.js
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+import Navbar from '../navbar.js'
+import { Box, Container } from '@chakra-ui/react'
+
+const Main = ({ children, router }) => {
+  return (
+    <Box as='main' pb={8}>
+      <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <title>Crișan Horea - homepage</title>
+      </Head>
+
+      <Navbar path={router.asPath} />
+
+      <Container maxW='container.md' pt={24}>
+        {children}
+      </Container>
+      
+    </Box>
+  )
+}
+
+export default Main
---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import { Container, Box, Heading, useColorModeValue } from '@chakra-ui/react'
+
+const Page = () => {
+  return (
+    <Container>
+      <Box
+        borderRadius="lg"
+        mb={6}
+        p={3}
+        textAlign="center"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        css={{ backdropFilter: 'blur(10px)' }}
+      >
+        Greetings, I'm a developer based in Cluj-Napoca, România!
+      </Box>
+
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as='h2' variant='page-title'>
+            Crișan Horea
+          </Heading>
+          <p>Digital creator under Space Frogz (Mobile Apps / Photography / Design)</p>
+        </Box>
+
+      </Box>
+    </Container>
+  )
+}
+
+export default Page;
