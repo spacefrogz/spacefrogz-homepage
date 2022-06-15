10b28a94400dd904edd73cde04cad2d67f47788e
Horea Crisan <h22ea@icloud.com>
2022-06-16 00:39:12 +0300

    update next.js config file

--- next.config.js
+++ next.config.js
@@ -2,8 +2,4 @@ module.exports = {
     basePath: process.env.NEXT_PUBLIC_BASE_PATH,
     assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
     reactStrictMode: true,
-    images: {
-        loader: 'akamai',
-        path: '',
-    },
 }
