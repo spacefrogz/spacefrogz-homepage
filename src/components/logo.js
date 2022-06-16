391e801987f9811b4ac798be3173bf8d1b96e465
Horea Crisan <h22ea@icloud.com>
2022-06-16 13:58:27 +0300

    Updated img to chakra-ui img

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { Image } from '@chakra-ui/react'
 import { Text, useColorModeValue } from '@chakra-ui/react'
 import styled from '@emotion/styled'
 import { imagePrefix } from '../utils/strings'
@@ -24,7 +25,7 @@ const Logo = () => {
     <Link href='/'>
       <a>
         <LogoBox>
-          <img src={spaceFrogzImg} width={20} height={20} alt='logo' />
+          <Image src={spaceFrogzImg} boxSize='20px' objectFit='cover' alt='logo' />
           <Text
             color={useColorModeValue('gray.800', 'whiteAlpha.900')}
             fontFamily='M PLUS Rounded 1c'
