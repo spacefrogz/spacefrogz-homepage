7a09d29893cbad1a1cdf56b526cd24a310f4a851
horikffee <37018766+horikffee@users.noreply.github.com>
2022-06-17 18:41:12 +0000

    Deploying to gh-pages from @ spacefrogz/spacefrogz-homepage@0fefbd44592473d82201f5afd3cf099f65056122 🚀

---
+++ _next/static/ojbqy7q0EKMc-rwN26tN6/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-8f696dcf9b29a606.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
